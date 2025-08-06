b892d9710eac53fa7443b501d0bbb7edd4cff298
Vamsi-kms <vamsi.k@algorims.com>
2025-08-07 00:38:24 +0800

    changed the tfvars file name

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -9,7 +9,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = var.cluster_role_arn
 
   vpc_config {
-    subnet_ids         = var.subnet_ids
+    subnet_ids         = data.aws_subnets.selected.ids
     security_group_ids = [var.security_group_id]
   }
 }
