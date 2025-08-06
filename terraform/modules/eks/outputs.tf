9a922c4f156e5f5cb8ff597332bf327a1c2aa60d
Vamsi-kms <vamsi.k@algorims.com>
2025-08-07 00:35:03 +0800

    changed the tfvars file name

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -2,7 +2,7 @@ output "cluster_name" {
   value = aws_eks_cluster.example.name
 }
 output "subnet_ids" {
-  value = data.aws_subnets.selected.ids
+  value = var.subnet_ids
 }
 
 # output "cluster_endpoint" {
